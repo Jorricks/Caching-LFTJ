39f88c5bda415a2b49a64486fff3e43ab197a887
Jorricks <j.w.sleijster@student.tue.nl>
2017-03-17 10:26:17 +0100

    Fixed not interpreting correctly

--- src/LFTJ.java
+++ src/LFTJ.java
@@ -3,11 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lftj;
+package src;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 
 /**
  *
--- src/Relation.java
+++ src/Relation.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package lftj;
+package src;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
 
